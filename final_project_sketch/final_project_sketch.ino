e460e8863c1f84ead5c2ab458aaf5153a5f4f83c
unknown <kam22@rice.edu>
2024-04-03 16:02:44 -0500

    Added code for LCD

--- final_project_sketch/final_project_sketch.ino
+++ final_project_sketch/final_project_sketch.ino
@@ -1,15 +1,29 @@
+// utlrasonic sensor code
 const int trigPin = 8;
 const int echoPin = 10;
 long duration;
 int distanceCm, distanceInch;
 
+// LCD code
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3 ,2);
+const int switchPin = 6;
+
 void setup() {
+  // ultrasonic sensor code
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
+
+  // LCD code
+  lcd.begin(16,2);
+  lcd.print("My name is:");
+  lcd.setCursor(0,1);
+  lcd.print("NAME");
 }
 
 void loop() {
+  // ultrasonic sensor code
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
