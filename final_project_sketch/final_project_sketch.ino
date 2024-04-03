3053d6fa6f431aea63fb4fd6a12d9d2c5adc5fc1
unknown <kam22@rice.edu>
2024-04-03 15:57:15 -0500

    Added code for ultrasonic sensor

--- final_project_sketch/final_project_sketch.ino
+++ final_project_sketch/final_project_sketch.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+const int trigPin = 8;
+const int echoPin = 10;
+long duration;
+int distanceCm, distanceInch;
 
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * 0.034 / 2;
+  distanceInch = duration * 0.0133 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
 
 }
